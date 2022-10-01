dea7a0751513df1ac894cb1ce2fb9858e6b979db
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-10-01 05:31:22 -0500

    shipped it!

--- shippit.sh
+++ shippit.sh
@@ -1,5 +1,5 @@
-dotnet publish ./Effluvium/Effluvium.vbproj -o ./pub-linux -c Release --sc -r linux-x64
-dotnet publish ./Effluvium/Effluvium.vbproj -o ./pub-windows -c Release --sc -r win-x64
+dotnet publish ./src/Effluvium/Effluvium.vbproj -o ./pub-linux -c Release --sc -r linux-x64
+dotnet publish ./src/Effluvium/Effluvium.vbproj -o ./pub-windows -c Release --sc -r win-x64
 butler push pub-windows thegrumpygamedev/effluvium:windows
 butler push pub-linux thegrumpygamedev/effluvium:linux
 git add -A
