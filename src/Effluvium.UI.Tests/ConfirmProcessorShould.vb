ddee1fc3bba4ce3df652aa08878d22e5a6587a34
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-10-01 06:17:42 -0500

    moved prompt setup outside of function

--- src/Effluvium.UI.Tests/ConfirmProcessorShould.vb
+++ src/Effluvium.UI.Tests/ConfirmProcessorShould.vb
@@ -1,10 +1,16 @@
 ﻿Public Class ConfirmProcessorShould
+    Private Sub SetupPrompt(UI As Mock(Of IUI), ParamArray choices As String())
+        Dim promptResults As New Queue(Of String)(choices)
+        UI.Setup(
+            Function(x) x.Prompt(It.IsAny(Of String), It.IsAny(Of String()))).
+            Returns(Function(a, b) promptResults.Dequeue())
+    End Sub
     <Theory>
     <InlineData(Choices.YesChoice, True)>
     <InlineData(Choices.NoChoice, False)>
     Sub ReturnAppropriatelyBasedOnTheChoiceOfYesOrNo(choice As String, expected As Boolean)
         Dim ui As New Mock(Of IUI)
-        ui.Setup(Function(x) x.Prompt(It.IsAny(Of String), It.IsAny(Of String()))).Returns(choice)
+        SetupPrompt(ui, choice)
         ConfirmProcessor.Confirm(ui.Object, "").ShouldBe(expected)
         ui.Verify(Function(x) x.Prompt(It.IsAny(Of String), It.IsAny(Of String())))
         ui.VerifyNoOtherCalls()
